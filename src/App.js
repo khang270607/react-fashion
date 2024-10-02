34e7e73a32b97282d2f2b9a7d52762b2c882e3c3
Huỳnh Vũ Khang <146689046+khang270607@users.noreply.github.com>
2024-10-02 07:50:51 +0700

    push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import DefaultLayout from './layouts/DefaultLayout';
+import Home from './views/Home/Home';
+import About from './views/About/About';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<DefaultLayout><Home /></DefaultLayout>} />
+          <Route path="/about" element={<DefaultLayout><About /></DefaultLayout>} />
+        </Routes>
+      </Suspense>
+    </Router>
   );
-}
+};
 
 export default App;
